dbd1f7ccead8989740260001535ced36927a42d3
Hsule-git <harshavardhansule@yahoo.com>
2025-09-30 22:35:01 +1000

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,11 +1,21 @@
-export function createElem(tag, classNames = [], innerHTML = "") {
+function createElem(tag, classNames = [], innerHTML = "") {
   const el = document.createElement(tag);
   classNames.forEach(c => el.classList.add(c));
-  if (innerHTML) el.innerHTML = innerHTML;
+  if (innerHTML) {
+    el.innerHTML = innerHTML;
+  }
   return el;
 }
 
-export function scrollToSelector(selector) {
+function scrollToSelector(selector) {
   const el = document.querySelector(selector);
-  if (el) el.scrollIntoView({ behavior: "smooth" });
+  if (el) {
+    el.scrollIntoView({ behavior: "smooth" });
+  }
 }
+
+// Expose under a global object
+window.utils = {
+  createElem,
+  scrollToSelector
+};
